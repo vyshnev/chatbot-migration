a7d47bf99fb4d5e5e64eff60fc40f276de3cbc4a
Vyshnev <vyshnevnandanam@gmail.com>
2026-05-11 12:22:19 +0530

    changed the search tool from duckduckgo to tavily - as duckduckgo was extremely unreliable

--- tools/search.py
+++ tools/search.py
@@ -4,16 +4,22 @@ tools/search.py
 Web search tool using DuckDuckGo with Redis caching.
 """
 
-from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_tavily import TavilySearch
 from langchain_core.tools import tool
 from cache.service import cached
 
-_raw_search = DuckDuckGoSearchRun(region="us-en")
+# Max results of 3 is usually perfect for chatbots
+_raw_search = TavilySearch(max_results=3)
 
 
 @tool
 def search_tool(query: str) -> str:
     """
-    Search the web for information. Use this when you need up-to-date facts.
+    Search the web for information using Tavily. Use this when you need up-to-date facts.
     """
-    return cached("search_tool", _raw_search.invoke, 7200, query)
+    def fetch_tavily(q):
+        # Tavily returns a list of dictionaries; we cast it to string for the LLM
+        results = _raw_search.invoke({"query": q})
+        return str(results)
+        
+    return cached("search_tool", fetch_tavily, 7200, query)
